9fb974a8c9545b2a866e40b75aa2605c31de51d8
Cyber Elf <cyberelf@example.com>
2026-07-13 13:04:33 -0400

    ci: add commitlint with conventional commit enforcement

---
+++ web/commitlint.config.js
@@ -0,0 +1,8 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'ci']],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 72]
+  }
+};
